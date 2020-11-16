ba9097c89338c3cbc79cb6ecbf30a8dac022df6b
Mohd Shaib <shaj.native@gmail.com>
2020-11-16 16:11:26 +0530

    final change

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,14 @@ import ToDoList from "./toDoList";
 function App() {
   const [inputText, setInputText] = React.useState("");
   const [itemList, setItemList] = React.useState([]);
+  const [editText, setEditText] = React.useState("");
 
   const handleChange = (event) => {
     let input = event.target.value;
     setInputText(input);
   };
 
-  const handleClick = () => {
+  const handleAdd = () => {
     if (!inputText) {
       return;
     }
@@ -21,27 +22,49 @@ function App() {
     setInputText("");
   };
 
-  const handleDelete = (name) => {
-    //console.log(name);
+  const handleEditChange = (event) => {
+    let edit = event.target.value;
+    setEditText(edit);
+  };
+
+  const handleSave = (id) => {
+    if (!editText) {
+      return;
+    }
     let arr = [...itemList];
-    //console.log(arr);
+    arr[id] = editText;
+    setItemList(arr);
+    setEditText("");
+  };
+
+  const handleDelete = (name) => {
+    let arr = [...itemList];
     let filterArr = arr.filter((item) => item !== name);
-    // console.log(filterArr);
     setItemList(filterArr);
   };
 
   return (
     <div id="main">
-      <input type="text" id="task" onChange={handleChange} value={inputText} />
-      <button id="btn" onClick={handleClick}>
+      <input
+        type="text"
+        id="task"
+        onChange={handleChange}
+        value={inputText}
+        placeholder="Enter task"
+      />
+      <button id="btn" onClick={handleAdd}>
         Add
       </button>
       <ul>
         {itemList.map((item, index) => (
           <ToDoList
             key={item + index}
+            id={index}
             itemName={item}
             handleDelete={handleDelete}
+            handleEditChange={handleEditChange}
+            editText={editText}
+            handleSave={handleSave}
           />
         ))}
       </ul>
--- src/components/toDoList.js
+++ src/components/toDoList.js
@@ -2,15 +2,45 @@ import React from "react";
 import "./../styles/App.css";
 
 function ToDoList(props) {
-  const { itemName, handleDelete } = props;
+  const {
+    id,
+    itemName,
+    handleDelete,
+    handleEditChange,
+    editText,
+    handleSave
+  } = props;
+  const [display, setDisplay] = React.useState(false);
+  const handleEdit = () => {
+    setDisplay(true);
+  };
   return (
     <>
       <li className="list">{itemName}</li>
       <div>
+        <button className="edit" onClick={handleEdit}>
+          Edit
+        </button>
         <button className="delete" onClick={() => handleDelete(itemName)}>
           Delete
         </button>
       </div>
+      {display ? (
+        <div>
+          <input
+            type="text"
+            className="editTask"
+            onChange={(event) => handleEditChange(event)}
+            value={editText}
+            placeholder="Edit task"
+          />
+          <button className="editSave" onClick={() => handleSave(id)}>
+            Save
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
     </>
   );
 }
