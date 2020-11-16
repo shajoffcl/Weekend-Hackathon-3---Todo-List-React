2053eff811f0021bd85159f87297123825fda2b9
Mohd Shaib <shaj.native@gmail.com>
2020-11-16 12:49:29 +0530

    changes done

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import "./../styles/App.css";
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
-}
+function App() {
+  const [itemName, setItemName] = React.useState("");
 
+  const handleChange = (event) => {
+    let input = event.target.value;
+    setItemName(input);
+  };
+  return (
+    <div id="main">
+      <input type="text" id="task" onChange={handleChange} value={itemName} />
+      <button id="btn" onClick="handleClick">
+        Add
+      </button>
+    </div>
+  );
+}
 
 export default App;
